c3852664e565d9ee01082b462a56349ff4b93086
GU3TT <duplouy.gaetan@laposte.net>
2022-12-02 01:51:04 +0100

    fix : conséquences d'une utilisation d'une carte dans le store

--- src/store/gamestore.ts
+++ src/store/gamestore.ts
@@ -1,11 +1,11 @@
-import {Card} from "../Card";
+import {BonusCard, Card, CardFamily, JokerCard, MalusCard, ScoreCard} from "../Card";
 import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 import {Player} from "../Player";
 
 export interface PlayerState {
     score: number,
     topCard: Card | undefined,
-    jockers: Card[],
+    jokers: JokerCard[],
     hand: Card[]
 }
 
@@ -15,21 +15,7 @@ export interface GameStore {
     deck: Card[]
 }
 
-const initialState: GameStore = {
-    playerA: {
-        score: 0,
-        topCard: undefined,
-        jockers: [],
-        hand: []
-    },
-    playerB: {
-        score: 0,
-        topCard: undefined,
-        jockers: [],
-        hand: []
-    },
-    deck: []
-}
+
 
 interface AddScorePayloadAction {
     player: Player,
@@ -53,7 +39,66 @@ interface PickCardPayloadAction {
 const initNumberCard: number = 6
 
 function getInitCard(): Card[] {
-    return []
+    let cards: Card[] = []
+    for (let i = 0; i < 5; i++) {
+        cards.push(new MalusCard("malus_preservatif.jpg", "Préservatif défectueux", CardFamily.preservatif))
+    }
+    for (let i = 0; i < 14; i++) {
+        cards.push(new BonusCard("bonus_preservatif.jpg", "Préservatif", CardFamily.preservatif))
+    }
+    for (let i = 0; i < 6; i++) {
+        cards.push(new BonusCard("bonus_situation.jpg", "Vivre Seul", CardFamily.situation))
+        cards.push(new BonusCard("bonus_syphilis.jpg", "Traitement anti-biotique", CardFamily.syphilis))
+        cards.push(new BonusCard("bonus_hpv.jpg", "Traitement par cryothérapie", CardFamily.hpv))
+        cards.push(new BonusCard("bonus_hepatite.jpg", "Guérison Spontanée", CardFamily.hepatiteB))
+    }
+    for (let i = 0; i < 4; i++) {
+        cards.push(new MalusCard("malus_situation.jpg", "Vivre en collectivité", CardFamily.situation))
+    }
+    for (let i = 0; i < 3; i++) {
+        cards.push(new MalusCard("malus_syphilis.jpg", "Syphilis", CardFamily.syphilis))
+        cards.push(new MalusCard("malus_hpv.jpg", "HPV", CardFamily.hpv))
+        cards.push(new MalusCard("malus_hepatite.jpg", "Hepatite D", CardFamily.hepatiteB))
+    }
+
+
+    cards.push(new MalusCard("joker_situation.jpg", "Vivre en  couple", CardFamily.situation))
+    cards.push(new MalusCard("joker_syphilis.jpg", "Recherche", CardFamily.syphilis))
+    cards.push(new MalusCard("joker_hpv.jpg", "Vaccin HPV", CardFamily.hpv))
+    cards.push(new MalusCard("joker_hepatite.jpg", "Vaccin Hepatite B", CardFamily.hepatiteB))
+
+    for (let i = 0; i < 10; i++) {
+        cards.push(new ScoreCard("score_25.jpg", "Score 25", 25, CardFamily.score))
+        cards.push(new ScoreCard("score_50.jpg", "Score 50", 50, CardFamily.score))
+        cards.push(new ScoreCard("score_75.jpg", "Score 75", 75, CardFamily.score))
+    }
+    for (let i = 0; i < 12; i++) {
+        cards.push(new ScoreCard("score_100.jpg", "Score 100", 100, CardFamily.score))
+    }
+
+    for (let i = 0; i < 4; i++) {
+        cards.push(new ScoreCard("score_200.jpg", "Score 200", 200, CardFamily.score))
+    }
+
+    return cards.sort((a, b) => 0.5 - Math.random());
+}
+
+const initCards = getInitCard()
+
+const initialState: GameStore = {
+    playerA: {
+        score: 0,
+        topCard: undefined,
+        jokers: [],
+        hand: initCards.slice(0, initNumberCard)
+    },
+    playerB: {
+        score: 0,
+        topCard: undefined,
+        jokers: [],
+        hand: initCards.slice(0, initNumberCard)
+    },
+    deck: initCards.slice(initNumberCard * 2, initCards.length)
 }
 
 export const gameSlice = createSlice({
@@ -68,18 +113,60 @@ export const gameSlice = createSlice({
             }
         },
         useCard: (state, action: PayloadAction<UseCardPayloadAction>) => {
-            //TODO Type de carte
-            if (action.payload.player == Player.playerA) {
-                state.playerA.topCard = action.payload.card
-            } else {
-                state.playerB.topCard = action.payload.card
+
+            if (action.payload.card instanceof BonusCard) {
+
+                state.deck.push(action.payload.card)
+                state.deck = state.deck.sort((a, b) => 0.5 - Math.random()); // Shuffle
+
+                if (action.payload.player == Player.playerA) {
+                    state.playerA.hand = state.playerA.hand.filter(x => x != action.payload.card)
+                    state.playerA.topCard = action.payload.card
+                } else {
+                    state.playerB.hand = state.playerB.hand.filter(x => x != action.payload.card)
+                    state.playerB.topCard = action.payload.card
+                }
+            } else if (action.payload.card instanceof MalusCard) {
+
+                state.deck.push(action.payload.card)
+                state.deck = state.deck.sort((a, b) => 0.5 - Math.random()); // Shuffle
+
+                if (action.payload.player == Player.playerA) {
+                    state.playerA.hand = state.playerA.hand.filter(x => x != action.payload.card)
+                    state.playerB.topCard = action.payload.card
+                } else {
+                    state.playerB.hand = state.playerB.hand.filter(x => x != action.payload.card)
+                    state.playerA.topCard = action.payload.card
+                }
+            } else if (action.payload.card instanceof ScoreCard) {
+
+                if (action.payload.player == Player.playerA) {
+                    state.playerA.hand = state.playerA.hand.filter(x => x != action.payload.card)
+                    state.playerA.score += action.payload.card.score
+                } else {
+                    state.playerB.hand = state.playerB.hand.filter(x => x != action.payload.card)
+                    state.playerB.score += action.payload.card.score
+                }
+
+            } else if (action.payload.card instanceof JokerCard) {
+
+                if (action.payload.player == Player.playerA) {
+                    state.playerA.hand = state.playerA.hand.filter(x => x != action.payload.card)
+                    state.playerA.jokers.push(action.payload.card)
+                } else {
+                    state.playerA.hand = state.playerA.hand.filter(x => x != action.payload.card)
+                    state.playerB.jokers.push(action.payload.card)
+                }
+
             }
+
+
         },
         reset: (state) => {
             state.playerA.score = 0
             state.playerB.score = 0
-            state.playerA.jockers = []
-            state.playerB.jockers = []
+            state.playerA.jokers = []
+            state.playerB.jokers = []
             state.playerA.topCard = undefined
             state.playerB.topCard = undefined
 
