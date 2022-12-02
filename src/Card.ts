7dad6f7e2d8be9e0a9b9a690544c585838fb1fec
GU3TT <duplouy.gaetan@laposte.net>
2022-12-02 01:45:25 +0100

    feat : création des cartes
    refs : #9 #10 #11 #12

--- src/Card.ts
+++ src/Card.ts
@@ -1,4 +1,123 @@
+import {Player} from "./Player";
+import {useAppDispatch, useAppSelector} from "./store/hook";
+import {useCard} from "./store/gamestore";
+
+export enum CardFamily {
+    situation,
+    syphilis,
+    hpv,
+    hepatiteB,
+    preservatif,
+    score
+}
+
 export interface Card {
     name: string,
-    image: string
+    image: string,
+    family : CardFamily
+
+    execute(player: Player): Promise<boolean>;
 }
+
+export class MalusCard implements Card {
+    constructor(image: string, name: string,family : CardFamily) {
+        this.image = image;
+        this.name = name;
+        this.family = family;
+    }
+
+    image: string;
+    name: string;
+    family : CardFamily;
+
+    async execute(player: Player): Promise<boolean> {
+        const topCard = useAppSelector(state => player == Player.playerA ? state.game.playerA.topCard : state.game.playerB.topCard)
+        if (!(topCard instanceof BonusCard))
+            return false;
+        const dispatcher = useAppDispatch()
+
+        dispatcher(useCard({player: player, card: this}))
+
+        return true;
+    }
+}
+
+
+export class BonusCard implements Card {
+
+    constructor(image: string, name: string,family : CardFamily) {
+        this.image = image;
+        this.name = name;
+        this.family = family;
+    }
+
+    image: string;
+    name: string;
+    family : CardFamily;
+
+    async execute(player: Player): Promise<boolean> {
+        const topCard = useAppSelector(state => player == Player.playerA ? state.game.playerA.topCard : state.game.playerB.topCard)
+
+        if (!(topCard instanceof MalusCard))
+            return false;
+
+
+        const dispatcher = useAppDispatch()
+
+        dispatcher(useCard({player: player, card: this}))
+
+
+        return true;
+    }
+
+}
+
+export class JokerCard implements Card {
+
+    constructor(image: string, name: string,family : CardFamily) {
+        this.image = image;
+        this.name = name;
+        this.family = family;
+    }
+
+    image: string;
+    name: string;
+    family : CardFamily;
+
+    async execute(player: Player): Promise<boolean> {
+
+        const dispatcher = useAppDispatch()
+
+        dispatcher(useCard({player: player, card: this}))
+
+
+        return true;
+    }
+
+}
+
+export class ScoreCard implements Card {
+
+    constructor(image: string, name: string, score: number,family : CardFamily) {
+        this.image = image;
+        this.name = name;
+        this.score = score;
+        this.family = family;
+    }
+
+    image: string;
+    score: number;
+    name: string;
+    family : CardFamily;
+
+    async execute(player: Player): Promise<boolean> {
+
+        const dispatcher = useAppDispatch()
+
+        dispatcher(useCard({player: player, card: this}))
+
+        return true;
+    }
+
+}
+
